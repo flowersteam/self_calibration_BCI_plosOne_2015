6cfcd80de929d837b4788400d60b309c04a0fcf5
Jonathan Grizou <jgrizou@jgrizou-MacPro.(none)>
2015-04-29 18:59:57 +0200

    minor change in scripts to be adapted with the readme instructions

--- offline/samplingEEG/generate_all_tables.m
+++ offline/samplingEEG/generate_all_tables.m
@@ -1,7 +1,4 @@
 %%
-analysis
-
-%%
 
 [pathstr, ~, ~] = fileparts(mfilename('fullpath'));
 plotFolder = fullfile(pathstr, 'plots');
@@ -9,7 +6,7 @@ if ~exist(plotFolder, 'dir')
    mkdir(plotFolder)
 end
 
-% generate_table(concatenate_calib_results('_150'))
-% generate_table(concatenate_calib_results('_300'))
-% generate_table(concatenate_calib_results('_custom'))
+generate_table(concatenate_calib_results('_150'))
+generate_table(concatenate_calib_results('_300'))
+generate_table(concatenate_calib_results('_custom'))
 generate_table(concatenate_calib_results('_self'))
